c1e52f348d1b721233ded379a2af8cfc337743ba
Joel Ottosson <joel.w.ottosson@gmail.com>
2017-06-09 16:28:31 +0200

    IOput --> 01_InOutput, by creating a new file

---
+++ Introduction/01_InOutput/01_InOutput/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>  			// Includes the library "iostream" - input and 
+#include <string>
+using namespace std;			// Is a standard on how to do the syntax from the library
+
+int main() {
+	string a = "Hay hay";
+	cout << a << endl;
+	cin >> a;
+	cout << a;
+	cin.ignore();
+	cin.get();
+}
