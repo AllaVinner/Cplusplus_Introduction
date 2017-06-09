56dfbf40e451e3e98cac262139e2d8dcff796ef1
Joel Ottosson <joel.w.ottosson@gmail.com>
2017-06-09 15:17:18 +0200

    Initial commit

---
+++ Introduction/Introduction/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>  			// Includes the library "iostream" - input and 
+using namespace std;			// Is a standard on how to do the syntax from the library
+
+int main() {
+
+	cout << "Hello world!!";			// Print on the screen using the iostream library
+	cin.ignore();
+	cin.get();
+}
