22a7b6b551bf59586b5aa5e77935cf178b8055bb
Joel Ottosson <joel.w.ottosson@gmail.com>
2017-06-15 07:39:19 +0200

    Added part of the string handler of input

--- Introduction/Roulette/Roulette/main.cpp
+++ Introduction/Roulette/Roulette/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -7,6 +8,8 @@ int main() {
 		// Odd is red and even numbers are black
 		const bool red = false;
 		const bool black = true;
+		const int number = 1;
+		const int colour = 2;
 		const int startingBalance = 1000;
 		const int numberMult = 10;
 		const int colourMult = 2;
@@ -17,6 +20,7 @@ int main() {
 		int guess;
 		bool colour;
 		bool gamOver = false;
+		string input;
 
 	// Welcome to the casino
 		cout << "Welcome to the wacky tacky macky Casino. You will start with a plesent gift of " << startingBalance
@@ -24,11 +28,26 @@ int main() {
 		
 	// Stake this will be the loop
 		cout << "How much do you want to bet? \n1. 100 \n2. 300 \n3. 500 \nYou choose: ";
-		cin >> guess;
-		cout << "Well " << guess << " money it is :) \n Do you want to bet on colour or a number? \n"
+		while (true) {
+			cin >> input;
+			if (stoi(input) == 1) stake = 100;
+			if (stoi(input) == 2) stake = 300;
+			if (stoi(input) == 3) stake = 500;
+		}
+
+	// What will we play with numbee or colour
+		cout << "Well " << stake << " money it is :) \n Do you want to bet on colour or a number? \n"
 			 << "1. Colour \n2. Number"
 			 << "The multiplication thing is " << numberMult << " for numbers and " << colourMult << " for colours.\n";
+		while (true) {
+			cin >> input;
+			if (stoi(input) == colour) {
+				cout << "Colour it is, do you go for black or red?\n1. Black\n2. Red\nIt is 50/50: ";
+				cin >> input;
+				
+			}
 
+		}
 
 	
 		cin.ignore();
