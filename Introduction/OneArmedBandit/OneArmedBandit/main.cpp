b5014ce778ff7d6125c6f89e82d2596fa4273aef
Joel Ottosson <joel.w.ottosson@gmail.com>
2017-08-08 15:54:05 +0200

    Armed bandit works, with one strange bug

--- Introduction/OneArmedBandit/OneArmedBandit/main.cpp
+++ Introduction/OneArmedBandit/OneArmedBandit/main.cpp
@@ -9,15 +9,18 @@ const int RANGE = 3;
 int inputHandler(string str);
 void printArray(int a[ROW][COLUMN]);
 void setArray(int(&a)[ROW][COLUMN]);
+int numberOfWins(int board[ROW][COLUMN]);
 
 int main() {
 
 	const int NUMSIGN = 3;
 	const int MAXROW = ROW * 2 + 2;
 
+
 	int balance;
 	int stake;
-	int rowCount;
+	int wins;
+	int tempNum;
 
 	int factors[MAXROW];
 	int board[ROW][COLUMN];
@@ -25,6 +28,17 @@ int main() {
 	bool gameOver = false;
 
 	string input;
+
+	//Initialize factors
+	for (int i = 0; i < MAXROW; i++) {
+		factors[i] = 1;
+	}
+	factors[0] = -1;
+	factors[1] = 1;
+	factors[2] = 3;
+	factors[3] = 7;
+	factors[4] = 15;
+	factors[8] = 127;
 
 	cout << "Hello my friend! Welcome back, we have missed you.\n"
 		<< "Today we will play One-Armed-Bandit!\nMay the odds be ever in your favour.\n";
@@ -35,7 +49,7 @@ int main() {
 		cin >> input;
 		balance = inputHandler(input);
 		if (0 < balance && balance <= 1000 && (balance % 20 == 0)) break;
-		cout << "Please select a value within the range!\n";
+		cout << "Please select an appropriate value!\n";
 	}
 
 	do {
@@ -45,17 +59,61 @@ int main() {
 			cin >> input;
 			stake = inputHandler(input);
 			if (1 <= stake && stake <= 50 && stake <= balance) break;
-			cout << "Please select a value within the range!\n";
+			cout << "Please select an appropriate value!\n";
 		}
 
 		//Start the game and colculate all the scores
 
 		setArray(board);
+		printArray(board);
+		wins = numberOfWins(board);
+		balance += factors[wins] * stake;
 
+		// Give the player the results
+		if (wins != 0) {
+			cout << "Congrtulations, you got " << wins << " you won " << factors[wins] * stake << ". ";
+		}
+		else {
+			cout << "Sorry you didn't get any rows. You lost " << stake << ". ";
+		}
+		cout << "This brings your balance to " << balance << ".\n";
 
-	} while (gameOver);
+		//Check if player has positive balance and wants/can play again
+		if (balance <= 0) {
+			gameOver = true;		// Player is forced to quit
+		}
+		else {
+			while (true) {
+				// Handling input to gameOver
+				cout << "Do you want to play again?\n1. Yes\n2. No\n: ";
+				cin >> input;
+				tempNum = inputHandler(input);
+				if (1 <= tempNum && tempNum <= 2) break;
+				cout << "Please select a value within the range!\n";
+			}
 
-	printArray(board);
+			// gameOver is assigned a value based on the input
+			switch (tempNum)
+			{
+			case 1:
+				gameOver = false;
+				break;
+			case 2:
+				gameOver = true;
+				break;
+			} // End of switch
+		} //End of if
+
+	} while (! gameOver);
+
+	//Casino saying godbye
+	if (balance > 0) {
+		cout << "So sorry to see you leaving, but well played.\nYou will be walking home with " << balance << ". That is not ashame\n";
+	}
+	else {
+		cout << "Get out of here you know body and come back when you have money you rat!\n";
+	}
+
 
 	cin.ignore();
 	cin.get();
@@ -95,4 +153,43 @@ void printArray(int a[ROW][COLUMN]) {
 		}
 		cout << endl;
 	}
+}
+
+int numberOfWins(int board[ROW][COLUMN]) {
+	int numberOfWins = 0;	 // Keeps track of how many winning rows have already been counted
+	int tempCount = 0;		 // Keeps track of how many cells is equal to the first one in that row.
+
+	// Check if there are wins in each row
+	for (int row = 0; row < ROW; row++) {
+		tempCount = 1;
+		for (int col = 1; col < COLUMN; col++) {
+			if (board[row][0] == board[row][col]) tempCount++;
+		}	
+		if (tempCount == COLUMN) numberOfWins++;
+	}
+
+	// Check if there are wins in each column
+	for (int col = 0; col < COLUMN; col++) {
+		tempCount = 1;
+		for (int row = 1; row < ROW; row++) {
+			if (board[0][col] == board[row][col]) tempCount++;
+		}
+		if (tempCount == ROW) numberOfWins++;
+	}
+
+	// Check diagonal from top-left to bot-right
+	tempCount = 1;
+	for (int d = 1; d < ROW; d++) {
+		if (board[0][0] == board[d][d]) tempCount++;
+	}
+	if (tempCount == ROW) numberOfWins++;
+
+	// Check diagonal from bot-right to top-left
+	tempCount = 1;
+	for (int d = 1; d < ROW; d++) {
+		if (board[ROW-1][0] == board[ROW-1-d][d]) tempCount++;
+	}
+	if (tempCount == ROW) numberOfWins++;
+
+	return numberOfWins;
 }
